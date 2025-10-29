17471861e8da6d08f69630f36335a8976cddf6a2
BulatovicBalsa <baki.b.2k02@gmail.com>
2025-10-29 17:04:37 +0100

    fix

--- src/MainTest.java
+++ src/MainTest.java
@@ -13,10 +13,12 @@ public class MainTest {
         MainTest testObj = new MainTest();
         int result = testObj.addNumbers(2, 3);
         int result2 = testObj.addNumbers(3, 5);
+        int result3 = testObj.addNumbers(3, 5);
 
 
         // Assert that the result of adding 2 and 3 equals 5
         assertEquals("Adding 2 and 3 should equal 5", 5, result);
         assertEquals("Adding 3 and 5 should equal 8", 8, result2);
+        assertEquals("Adding 3 and 5 should equal 8", 8, result3);
     }
 }
